5ff82bbb31c7e684b93d5795d9466cf4763c74f3
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-13 11:36:13 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -22,10 +22,7 @@ end
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
-  
-  songs.each do |key, value|
+  my_songs.keys.each_with_index do |song, index|
     puts "#{index+1}. #{song}"
   end
 
